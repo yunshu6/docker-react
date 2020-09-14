63d84b7925dbb3a4eb158c57aa6cf4e03baab07f
Yunshu <yliu@lyft.com>
2020-09-14 16:25:41 -0700

    change content in a feature branch

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload. 
           Reload successful !
+          Now on feature branch !
         </p>
         <a
           className="App-link"
